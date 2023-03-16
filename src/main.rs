08806957b0b92efcbe013358e0ac1e9a0187d886
Anish Khanijaur <anishkhanijaur089@gmail.com>
2023-03-16 15:19:51 +0700

    added assets directory

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use clap::Parser;
 use cli::Args;
+// use image::EncodableLayout;
+// use show_image::{create_window, ImageInfo, ImageView};
 
 mod cli;
 
 fn main() {
     let args: Args = Args::parse();
-    let img = image::open(args.img_path).expect("no file found lol");
-    dbg!(img);
+    let _img = image::open(args.img_path).expect("no file found aa");
 }
+
+// pre-commit run --all
