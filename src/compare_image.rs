8cf2761bb3a6b83b4d04444935a24fbf34801935
Anish Khanijaur <anishkhanijaur089@gmail.com>
2023-04-02 01:00:00 +0700

    Cleaned up code and new usage of edge detection

--- src/compare_image.rs
+++ src/compare_image.rs
@@ -5,7 +5,7 @@ use rand::Rng;
 pub struct ShapeInfo(u32, u32, u32, u32, [u8; 4]);
 
 impl ShapeInfo {
-    // TODO: Take in a palette
+    // Create a random shape
     pub fn make_random(
         max_x: u32,
         max_y: u32,
@@ -13,39 +13,37 @@ impl ShapeInfo {
         current_cost: u32,
         options: &Vec<[u8; 4]>,
     ) -> ShapeInfo {
+        // Create a random thread
         let mut rng = rand::thread_rng();
 
+        // Calculate the weighted max width and height for the shape
         let weighted_max_width =
             (max_x as f64 * (current_cost as f64 * 2.0_f64 / original_cost as f64)) as u32;
         let weighted_max_height =
             (max_y as f64 * (current_cost as f64 * 2.0_f64 / original_cost as f64)) as u32;
 
+        // Randomly select an (x, y) coordinate to place the shape
         let x = rng
-            .gen_range((0 - (weighted_max_width as f32 / 2.0) as i32)..(max_x as i32))
-            .max(0) as u32;
-
+            .gen_range((0 - (weighted_max_width as f32 / 2.0) as i32)..(max_x as i32)) // Generate random number
+            .max(0) as u32; // Ensure that that number is larger than 0
         let y = rng
-            .gen_range((0 - (weighted_max_height as f32 / 2.0) as i32)..(max_y as i32))
-            .max(0) as u32;
+            .gen_range((0 - (weighted_max_height as f32 / 2.0) as i32)..(max_y as i32)) // Generate random number
+            .max(0) as u32; // Ensure that that number is larger than 0
 
+        // Randomly select the width and height of the shape
         let width = rng.gen_range(1..(weighted_max_width + 3));
-
         let width = if x + width >= max_x {
             max_x - x
         } else {
             width - 1
         };
-
-        // println!("width: {}", width);
         let height = rng.gen_range(1..(weighted_max_height + 3));
-
         let height = if y + height >= max_y {
             max_y - y
         } else {
             height - 1
         };
 
-        // println!("height: {}", height);
         // ShapeInfo(x, y, width, height, [r, g, b, 255])
         ShapeInfo(
             x,
@@ -55,44 +53,47 @@ impl ShapeInfo {
             options[rng.gen_range(0..options.len())],
         )
     }
-    // for using edge to make shape
-    // pub fn make_using_edge(
-    //     max_x: u32,
-    //     max_y: u32,
-    //     original_cost: u32,
-    //     current_cost: u32,
-    //     options: &Vec<[u8; 4]>,
-    //     edge_detection: &Vec<(u32, u32)>,
-    // ) -> ShapeInfo {
-    //     let mut rng = rand::thread_rng();
-    //
-    //     let weighted_max_width =
-    //         (max_x as f64 * (current_cost as f64 * 2.0_f64 / original_cost as f64)) as u32;
-    //     let weighted_max_height =
-    //         (max_y as f64 * (current_cost as f64 * 2.0_f64 / original_cost as f64)) as u32;
-    //     let (x, y) = edge_detection[rng.gen_range(0..edge_detection.len())];
-    //     let width = rng.gen_range(1..(weighted_max_width + 3));
-    //     let width = if x + width >= max_x {
-    //         max_x - x
-    //     } else {
-    //         width - 1
-    //     };
-    //     let height = rng.gen_range(1..(weighted_max_height + 3));
-    //     let height = if y + height >= max_y {
-    //         max_y - y
-    //     } else {
-    //         height - 1
-    //     };
-    //     ShapeInfo(
-    //         x,
-    //         y,
-    //         width,
-    //         height,
-    //         options[rng.gen_range(0..options.len())],
-    //     )
-    // }
+    // Create a random shape at a coordinate following the logic above
+    /*
+    pub fn make_using_edge(
+        max_x: u32,
+        max_y: u32,
+        original_cost: u32,
+        current_cost: u32,
+        options: &Vec<[u8; 4]>,
+        coords: (u32, u32),
+    ) -> ShapeInfo {
+        let mut rng = rand::thread_rng();
+
+        let weighted_max_width =
+            (max_x as f64 * (current_cost as f64 * 2.0_f64 / original_cost as f64)) as u32;
+        let weighted_max_height =
+            (max_y as f64 * (current_cost as f64 * 2.0_f64 / original_cost as f64)) as u32;
+        let (x, y) = coords;
+        let width = rng.gen_range(1..(weighted_max_width + 3));
+        let width = if x + width >= max_x {
+            max_x - x
+        } else {
+            width - 1
+        };
+        let height = rng.gen_range(1..(weighted_max_height + 3));
+        let height = if y + height >= max_y {
+            max_y - y
+        } else {
+            height - 1
+        };
+        ShapeInfo(
+            x,
+            y,
+            width,
+            height,
+            options[rng.gen_range(0..options.len())],
+        )
+    }
+     */
 }
 
+// Draw the shape onto the canvas
 pub fn draw(canvas: &mut DynamicImage, shape: ShapeInfo) {
     for y in shape.1..(shape.1 + shape.3) {
         for x in shape.0..(shape.0 + shape.2) {
@@ -107,6 +108,7 @@ pub fn draw(canvas: &mut DynamicImage, shape: ShapeInfo) {
     }
 }
 
+// Compare our proposed image to the target image
 pub fn compare_imaginary(
     target_img: &DynamicImage,
     canvas: &DynamicImage,
@@ -129,6 +131,7 @@ pub fn compare_imaginary(
     }
 }
 
+// Calculate the absolute differences in rgb values
 pub fn rgb_difference(img1: &[u8; 4], img2: &[u8; 4]) -> u32 {
     img1[0].abs_diff(img2[0]) as u32
         + img1[1].abs_diff(img2[1]) as u32
@@ -136,6 +139,7 @@ pub fn rgb_difference(img1: &[u8; 4], img2: &[u8; 4]) -> u32 {
         + img1[3].abs_diff(img2[3]) as u32
 }
 
+// Calculate the absolute differences in rgb values between two images
 pub fn cost(img1: &DynamicImage, img2: &DynamicImage) -> u32 {
     let mut output: u32 = 0;
     for (x, y, rgba) in img1.pixels() {
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use crate::compare_image::{compare_imaginary, cost, draw, ShapeInfo};
+// use crate::edge_detection::edge_detection;
 use clap::Parser;
 use cli::Args;
 use image::{DynamicImage, GenericImageView};
@@ -6,20 +7,25 @@ use image::{DynamicImage, GenericImageView};
 use rayon::iter::*;
 use show_image::{create_window, ImageInfo, ImageView};
 use std::collections::HashSet;
-// use crate::edge_detection::edge_detection;
 // use std::time;
 
 mod cli;
 mod compare_image;
 // mod edge_detection;
 
+// pre-commit run --all
+
 #[show_image::main]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Get the image path
     let args: Args = Args::parse();
     let img = image::open(args.img_path).expect("no file found");
-    let mut output = DynamicImage::new_rgba8(img.width(), img.height());
 
+    // Create the canvas with the same image dimensions
+    let mut output = DynamicImage::new_rgba8(img.width(), img.height());
     let (width, height) = img.dimensions();
+
+    // Extract the colors out of the target image
     let colors = img
         .pixels()
         .map(|(_, _, r)| r.0)
@@ -28,7 +34,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .copied()
         .collect::<Vec<[u8; 4]>>();
 
-    // edge detection
+    // Use edge detection to find the edges in an image
     // let edge_detection: GrayImage = edge_detection(&img, width, height);
     // let edge_image: DynamicImage = edge_detection.into();
     // let (_w, _h) = edge_image.dimensions();
@@ -36,22 +42,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     //     .pixels()
     //     .flat_map(|(_, _, r)| r.0)
     //     .collect::<Vec<u8>>();
-    //
+
+    // Store the coordinates of the edges
     // let coordinate_of_picture = &edge_image
     //     .pixels().filter(|(_,_,r)| r.0 != [0,0,0,0]).map(|(x, y, _)| (x,y)).collect::<Vec<(u32, u32)>>();
-    // dbg!(coordinate_of_picture);
 
+    // Create a new image view
     let pixel_data = output
         .pixels()
         .flat_map(|(_, _, r)| r.0)
         .collect::<Vec<u8>>();
     let image = ImageView::new(ImageInfo::rgba8(width, height), &pixel_data);
-    //this is for the edge detection.
-    // let image = ImageView::new(ImageInfo::rgba8(_w, _h), &_p_a);
 
     // Create a window with default options and display the image.
     let window = create_window("image", Default::default())?;
     window.set_image("image-001", image)?;
+
+    // Calculate the original cost
     let original_cost = cost(&img, &output);
     let mut current_cost = original_cost;
     // println!("Cost: {}", current_cost);
@@ -59,10 +66,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // use std::time::Instant;
     // let now = Instant::now();
 
+    // The event loop that updates the window showing the canvas every time the canvas is drawn upon.
     // If the user closes the window, the channel is closed and the loop also exits.
     'outer: loop {
         let mut update_loop = || {
-            let v = (0..300)
+            // == Randomly generating the next best image ==
+            // Calculate the shape that reduces the current cost by the most amount
+            let max_cost_reduction = (0..9000)
                 .into_par_iter()
                 .map(|_| {
                     let shape =
@@ -72,6 +82,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 })
                 .max_by(|a, b| a.0.partial_cmp(&b.0).unwrap())
                 .unwrap();
+            // ==                                         ==
+
+            // == Using edge detection to generate the next best image ==
+            // Calculate the shape that reduces the current cost by the most amount
+            // let max_cost_reduction = coordinate_of_picture
+            // .into_par_iter()
+            // .map(|coords| {
+            //     let coords = (coords.0.to_owned(), coords.1.to_owned());
+            //     let shape =
+            //         ShapeInfo::make_using_edge(width, height, original_cost, current_cost, &colors, coords);
+            //     let diff = compare_imaginary(&img, &output, &shape);
+            //     (diff, shape)
+            // })
+            // .max_by(|a, b| a.0.partial_cmp(&b.0).unwrap())
+            // .unwrap();
+            // ==                                                      ==
+
             /* for timing */
             // let elapsed = now.elapsed();
             // println!("Elapsed: {:.2?}", elapsed);
@@ -80,9 +107,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             // }
             // dbg!(&v);
 
-            if v.0 > 0 {
-                draw(&mut output, v.1);
-                current_cost -= v.0;
+            // If the next best shape reduces the total cost, draw the image onto the canvas
+            if max_cost_reduction.0 > 0 {
+                draw(&mut output, max_cost_reduction.1);
+                current_cost -= max_cost_reduction.0;
                 let pixel_data = output
                     .pixels()
                     .flat_map(|(_, _, r)| r.0)
@@ -97,7 +125,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         };
         match window.event_channel() {
             Ok(_) => update_loop(),
-            Err(_) => break 'outer,
+            Err(_) => {
+                break 'outer;
+            }
         }
     }
 
